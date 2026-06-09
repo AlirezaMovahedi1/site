dde48de45d1011922fa32eb3472c33b964191a01
Alireza Movahedi <alireza82m02@gmail.com>
2026-06-09 21:15:29 +0330

    feat: replace laptop icon with custom geometric S corporate logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { useCart } from '../context/CartContext';
-import { ShoppingCart, Laptop } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 import ThemeToggle from './ThemeToggle';
 import styles from './Header.module.css';
 
@@ -14,7 +14,18 @@ export default function Header() {
     <header className={styles.header}>
       <div className={`container ${styles.headerContainer}`}>
         <Link href="/" className={styles.logo}>
-          <Laptop className={styles.logoIcon} />
+          <svg viewBox="0 0 100 110" className={styles.logoIcon} fill="currentColor">
+            {/* Top-Left Triangle */}
+            <polygon points="18,28 49,10 49,46" />
+            {/* Top-Right Triangle */}
+            <polygon points="82,28 51,10 51,46" />
+            {/* Middle Parallelogram */}
+            <polygon points="18,34 82,71 82,89 18,52" />
+            {/* Bottom-Left Triangle */}
+            <polygon points="18,82 49,100 49,64" />
+            {/* Bottom-Right Triangle */}
+            <polygon points="82,82 51,100 51,64" />
+          </svg>
           <div className={styles.logoText}>
             <h1>سیدآی‌تی مارکت</h1>
             <span>سخت‌افزار و لایسنس اورجینال</span>
